ad52a5b501997cef4940557b2cf36b6fda69527c
Weiqi <weiqizhang64@gmail.com>
2020-08-20 10:22:05 -0500

    constraint deletion bug fixes

--- examples/spatial_bac.jl
+++ examples/spatial_bac.jl
@@ -1,5 +1,5 @@
 using BranchAndBound, JuMP, PowerModels, DeNet
-using Mosek, MosekTools, Ipopt, COSMO
+using Mosek, MosekTools, Ipopt, COSMO, Gurobi
 using LinearAlgebra
 const BB = BranchAndBound
 const PM = PowerModels
@@ -39,7 +39,7 @@ mutable struct SpatialBCBranch <: BB.AbstractBranch
     wi::JuMP.VariableRef
     mod_branch::Union{Nothing, ComplexVariableBranch, BB.VariableBranch} # this capture either a changed bound for Wij and Tij, or a changed bound for Wii/Wjj 
     bounds::NTuple{6, <:Real} # Lii, Uii, Ljj, Ujj, Lij, Uij
-    valid_ineq_coeffs::NTuple{5, <:Real} # π coefficients # might not be needed
+    valid_ineq_coeffs::NTuple{5, <:Real} # π coefficients
 end
 
 # create a SpatialBCBranch, in which mod_branch is Nothing (the exact matrix entry to branch is not decided yet)
@@ -165,6 +165,13 @@ function find_branching_entry(branch::SpatialBCBranch, node::BB.AbstractNode)::T
     return opt_idx
 end
 
+function delete_prev_branch_constr!(model::JuMP.Model, node::BB.AbstractNode)
+    while !isempty(node.auxiliary_data["prev_branch_crefs"])
+        cref = Base.pop!(node.auxiliary_data["prev_branch_crefs"])
+        delete(model, cref)
+    end
+end
+
 function solve_candidate_nodes(branch::SpatialBCBranch, node::BB.AbstractNode)
     function _make_fake_branch(branch::SpatialBCBranch, mode::String)
         (Lii, Uii, Ljj, Ujj, Lij, Uij) = branch.bounds
@@ -188,18 +195,11 @@ function solve_candidate_nodes(branch::SpatialBCBranch, node::BB.AbstractNode)
         return μ * max(-λmin_up, -λmin_down) + (1-μ) * min(-λmin_up, -λmin_down)
     end
 
-    function delete_prev_branch_constr!(model::JuMP.Model, node::BB.AbstractNode)
-        for cref in node.auxiliary_data["prev_branch_crefs"]
-            delete(model, cref)
-        end
-        delete!(node.auxiliary_data, "prev_branch_crefs")
-    end
-
     i = branch.i
     j = branch.j
     root = find_root(node)
     model = root.model
-    backtracking!(model, node)
+    # backtracking!(model, node)
     modes = ["Wii", "Wjj", "Wij"]
     best_score = -Inf
     best_mode = ""
@@ -218,6 +218,7 @@ function solve_candidate_nodes(branch::SpatialBCBranch, node::BB.AbstractNode)
 
         # compute score, update if better
         score = compute_score(up_solution, down_solution)
+        println(score)
         if score > best_score
             best_score = score
             best_mode = mode
@@ -256,12 +257,12 @@ function add_constraints_from_branch!(model::JuMP.Model, branch::SpatialBCBranch
     wr = branch.wr
     wi = branch.wi
     new_branches = []
-    push!(new_branches, JuMP.@constraint(node.model, Lii <= wii <= Uii))
-    push!(new_branches, JuMP.@constraint(node.model, Ljj <= wjj <= Ujj))
-    push!(new_branches, JuMP.@constraint(node.model, Lij * wr <= wi))
-    push!(new_branches, JuMP.@constraint(node.model, wi <= Uij * wi))
-    push!(new_branches, JuMP.@constraint(node.model, πs[1] + πs[2] * wii + πs[3] * wjj + πs[4] * wr + πs[5] * wi >= Ujj * wii + Uii * wjj - Uii * Ujj))
-    push!(new_branches, JuMP.@constraint(node.model, πs[1] + πs[2] * wii + πs[3] * wjj + πs[4] * wr + πs[5] * wi >= Ljj * wii + Lii * wjj - Lii * Ljj))
+    push!(new_branches, JuMP.@constraint(model, Lii <= wii <= Uii))
+    push!(new_branches, JuMP.@constraint(model, Ljj <= wjj <= Ujj))
+    push!(new_branches, JuMP.@constraint(model, Lij * wr <= wi))
+    push!(new_branches, JuMP.@constraint(model, wi <= Uij * wi))
+    push!(new_branches, JuMP.@constraint(model, πs[1] + πs[2] * wii + πs[3] * wjj + πs[4] * wr + πs[5] * wi >= Ujj * wii + Uii * wjj - Uii * Ujj))
+    push!(new_branches, JuMP.@constraint(model, πs[1] + πs[2] * wii + πs[3] * wjj + πs[4] * wr + πs[5] * wi >= Ljj * wii + Lii * wjj - Lii * Ljj))
     return new_branches
 end
 
@@ -275,12 +276,16 @@ end
 
 function BB.branch!(tree::BB.AbstractTree, node::BB.AbstractNode)
     @info " Node id $(node.id), status $(node.solution_status), bound $(node.bound)"
+    root = find_root(node)
+    model = root.model
+    # print_model_to_file(model, "model_$(node.id)_before")
     if node.bound >= tree.best_incumbent
+        delete_prev_branch_constr!(model, node)
         @info " Fathomed by bound"
     elseif node.depth >= 25
+        delete_prev_branch_constr!(model, node)
         @info " Fathomed by maximum depth"
     elseif node.solution_status in [MOI.OPTIMAL, MOI.LOCALLY_SOLVED, MOI.SLOW_PROGRESS]
-        root = find_root(node)
         pm = root.auxiliary_data["PM"]
         (i,j) = find_min_eigen(node)
         new_sbc_branch = create_sbc_branch(i, j, node)
@@ -317,17 +322,21 @@ function BB.branch!(tree::BB.AbstractTree, node::BB.AbstractNode)
         next_branch_down.mod_branch = down_mod_branch
         child_up = BB.create_child_node(node, next_branch_up)
         child_down = BB.create_child_node(node, next_branch_down)
+        # child_up.model = root.model
+        # child_down.model = root.model
         BB.push!(tree, child_up)
         BB.push!(tree, child_down)
     else
+        delete_prev_branch_constr!(model, node)
         @info " Fathomed by solution status: $(node.solution_status)"
     end
+    # print_model_to_file(model, "model_$(node.id)_after")
 end
 
 # implement depth first rule
 function BB.next_node(tree::BB.AbstractTree)
     # best bound
-    # sort!(tree::BB.AbstractTree) = Base.sort!(tree.nodes, by=x->x.depth)
+    sort!(tree::BB.AbstractTree) = Base.sort!(tree.nodes, by=x->x.depth)
     BB.sort!(tree)
     node = Base.pop!(tree.nodes)
     return node
@@ -353,25 +362,91 @@ BB.adjust_branch!(branch_objects::Array{SpatialBCBranch,1}) = nothing
 BB.apply_changes!(node::BB.JuMPNode) = nothing
 
 function BB.bound!(node::BB.JuMPNode)
-    node.model = find_root(node).model
-    backtracking!(node.model, node)
-    JuMP.optimize!(node.model)
-    node.solution_status = JuMP.termination_status(node.model)
-
-    if node.solution_status in [MOI.OPTIMAL, MOI.LOCALLY_SOLVED, MOI.SLOW_PROGRESS]
-        node.bound = JuMP.objective_value(node.model)
-        vrefs = JuMP.all_variables(node.model)
+    model = find_root(node).model
+    # print_model_to_file(model, node.id)
+    println(total_num_constraints(model))
+    backtracking!(model, node)
+    JuMP.optimize!(model)
+    node.solution_status = JuMP.termination_status(model)
+    if node.solution_status == MOI.INFEASIBLE || JuMP.dual_status(model) in [MOI.INFEASIBILITY_CERTIFICATE]
+        node.bound = Inf
+    elseif node.solution_status == MOI.DUAL_INFEASIBLE || JuMP.primal_status(model) in [MOI.INFEASIBILITY_CERTIFICATE]
+        node.bound = -Inf
+    elseif node.solution_status in [MOI.OPTIMAL, MOI.LOCALLY_SOLVED, MOI.SLOW_PROGRESS]
+        node.bound = JuMP.objective_value(model)
+        vrefs = JuMP.all_variables(model)
         for v in vrefs
             node.solution[v] = JuMP.value(v)
         end
-    elseif node.solution_status == MOI.INFEASIBLE
-        node.bound = -Inf
-    elseif node.solution_status == MOI.DUAL_INFEASIBLE
-        node.bound = Inf
     else
         @warn "Unexpected node solution status: $(node.solution_status)"
         node.bound = -Inf
     end
+end
+
+function BB.heuristics!(node::BB.JuMPNode)
+    #=
+    ipopt_optimizer = optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0)
+    exact_model = copy(node.model)
+    buses = collect(keys(ref(pm, :bus)))
+    JuMP.@variable(exact_model, vr[buses])
+    JuMP.@variable(exact_model, vi[buses])
+
+    # get rid of PSDCone constraint (for Ipopt)
+    psdcone = JuMP.all_constraints(exact_model, Array{GenericAffExpr{Float64, VariableRef}, 1}, MOI.PositiveSemidefiniteConeSquare)[1]
+    JuMP.delete(exact_model, psdcone)
+
+    # add rank 1 constraints
+    for i in buses, j in buses
+        if i == j
+            Wi = JuMP.variable_by_name(exact_model, "0_WR[$(i),$(i)]")
+            Wj = JuMP.variable_by_name(exact_model, "0_WR[$(j),$(j)]")
+            JuMP.@constraint(exact_model, Wi == vr[i]^2 + vi[i]^2)
+            JuMP.@constraint(exact_model, Wj == vr[j]^2 + vi[j]^2)
+        else
+            if !isnothing(JuMP.variable_by_name(exact_model, "0_WR[$(i),$(j)]"))
+                WR = JuMP.variable_by_name(exact_model, "0_WR[$(i),$(j)]")
+            else
+                WR = JuMP.variable_by_name(exact_model, "0_WR[$(j),$(i)]")
+            end
+            WI = JuMP.variable_by_name(exact_model, "0_WI[$(i),$(j)]")
+            JuMP.@constraint(exact_model, WR == vr[i] * vr[j] + vi[i] * vi[j])
+            JuMP.@constraint(exact_model, WI == vr[j] * vi[i] - vi[j] * vr[i])
+        end
+    end
+
+    # convert second order cone constraints into quadratic constraints
+    soc_crefs = JuMP.all_constraints(exact_model, Array{GenericAffExpr{Float64, VariableRef}, 1}, MOI.SecondOrderCone)
+    for cref in soc_crefs
+        cobj = JuMP.constraint_object(cref)
+        terms = cobj.func
+        JuMP.@constraint(exact_model, sum(terms[i]^2 for i in 1:length(terms)) <= terms[1]^2)
+        JuMP.delete(exact_model, cref)
+    end
+
+    JuMP.set_optimizer(exact_model, ipopt_optimizer)
+    JuMP.optimize!(exact_model)
+    node.best_incumbent = JuMP.objective_value(exact_model)
+    JuMP.set_optimizer(node.model, optimizer)
+    =#
+end
+
+###########################################################################
+#                     Helper Functions for Debugging                      #
+###########################################################################
+
+function total_num_constraints(model::JuMP.Model)::Int64
+    sum = 0
+    for (i,j) in JuMP.list_of_constraint_types(model)
+        sum += num_constraints(model, i, j)
+    end
+    return sum
+end
+
+function print_model_to_file(model::JuMP.Model, id::String)
+    f = open(id, "w")
+    print(f, model)
+    close(f)
 end
 
 ###########################################################################
